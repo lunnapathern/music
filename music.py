bd9a53a4bc4fc536d1b5763b33c9c78f9bbe55f3
lunnapathern <46614905+lunnapathern@users.noreply.github.com>
2019-01-13 12:51:30 -0200

    Update music.py

--- music.py
+++ music.py
@@ -1,249 +1,124 @@
-import asyncio
 import discord
-from discord.ext import commands
+import random
+import youtube_dl
 import datetime
-#CRIADO POR ZERO
-#CODE FULL COGS MÚSICA
+client = discord.Client()
 
-color = 0x0cb4f1
+players = {}
+color = 0xe081e4
 
-if not discord.opus.is_loaded():
-    discord.opus.load_opus('opus')
-
-
-class VoiceEntry:
-    def __init__(self, message, player):
-        self.requester = message.author
-        self.channel = message.channel
-        self.player = player
-
-    def __str__(self):
-        fmt = '**<a:disco:495599105033109557> Música:**\n{0.title}\n**<:Online:484132926838276096> Música Solicitada Por:**\n{1.display_name}\n'
-        duration = self.player.duration
-        if duration:
-            fmt = fmt + ' **<a:carregando:489775219339165703> Duração da música:**\n{0[0]} **Minutos** / {0[1]} **Segundos**'.format(divmod(duration, 60))
-        return fmt.format(self.player, self.requester)
-
-class VoiceState:
-    def __init__(self, bot):
-        self.current = None
-        self.voice = None
-        self.bot = bot
-        self.play_next_song = asyncio.Event()
-        self.songs = asyncio.Queue()
-        self.skip_votes = set()
-        self.audio_player = self.bot.loop.create_task(self.audio_player_task())
-
-    def is_playing(self):
-        if self.voice is None or self.current is None:
-            return False
-
-        player = self.current.player
-        return not player.is_done()
-
-    @property
-    def player(self):
-        return self.current.player
-
-    def skip(self):
-        self.skip_votes.clear()
-        if self.is_playing():
-            self.player.stop()
-
-    def toggle_next(self):
-        self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
-
-    async def audio_player_task(self):
-        while True:
-            self.play_next_song.clear()
-            self.current = await self.songs.get()
-            embed1 = discord.Embed(color=color, description="**• Tocando agora:**\n\n" + str(self.current))
-            embed1.set_thumbnail(url="https://cdn.discordapp.com/attachments/510459921054040065/512242135760896012/tumblr_nkd4h5V0a91rzy0w8o1_500.gif")
-            embed1.set_footer(text="• Sona 白 🎧  ajuda? digite s!ajuda")
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.send_message(self.current.channel,embed=embed1)
-            self.current.player.start()
-            await self.play_next_song.wait()
-
-class Music:
-    def __init__(self, bot):
-        self.bot = bot
-        self.voice_states = {}
-
-    def get_voice_state(self, server):
-        state = self.voice_states.get(server.id)
-        if state is None:
-            state = VoiceState(self.bot)
-            self.voice_states[server.id] = state
-
-        return state
-
-    async def create_voice_client(self, channel):
-        voice = await self.bot.join_voice_channel(channel)
-        state = self.get_voice_state(channel.server)
-        state.voice = voice
-
-    def __unload(self):
-        for state in self.voice_states.values():
-            try:
-                state.audio_player.cancel()
-                if state.voice:
-                    self.bot.loop.create_task(state.voice.disconnect())
-            except:
-                pass
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def join(self, ctx, *, channel : discord.Channel):
-        try:
-            await self.create_voice_client(channel)
-        except discord.ClientException:
-            embed1 = discord.Embed(color=color, description="**• Já estou em um canal de voz!**")
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.say(embed=embed1)
-            await self.bot.say(embed=embed1)
-        except discord.InvalidArgument:
-            embed1 = discord.Embed(color=color, description="**• Este não é um canal de voz!**")
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.say(embed=embed1)
-        else:
-            await self.bot.say('Pronto para reproduzir audio em ' + channel.name)
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def summon(self, ctx):
-        summoned_channel = ctx.message.author.voice_channel
-        if summoned_channel is None:
-            embed1 = discord.Embed(color=color, description="**• Você não esta em um canal de voz!**")
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.say(embed=embed1)
-            return False
-
-        state = self.get_voice_state(ctx.message.server)
-        if state.voice is None:
-            state.voice = await self.bot.join_voice_channel(summoned_channel)
-        else:
-            await state.voice.move_to(summoned_channel)
-
-        return True
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def play(self, ctx, *, song : str):
-        state = self.get_voice_state(ctx.message.server)
-        opts = {
-            'default_search': 'auto',
-            'quiet': True,
-        }
-
-        if state.voice is None:
-            success = await ctx.invoke(self.summon)
-            if not success:
-                return
-
-        try:
-            player = await state.voice.create_ytdl_player(song, ytdl_options=opts, after=state.toggle_next)
-        except Exception as e:
-            fmt = '• Ocorreu um erro ao processar esta solicitação:{}: {}'
-        else:
-            player.volume = 0.1
-            entry = VoiceEntry(ctx.message, player)
-            embed1 = discord.Embed(color=color, description="**• Na Playlist**\n\n" + str(entry))
-            embed1.set_footer(text="• Sona 白 🎧 Música 24 horas / Bugs ? relate para o suporte")
-            await self.bot.say(embed=embed1)
-            await state.songs.put(entry)
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def volume(self, ctx, value : int):
-        state = self.get_voice_state(ctx.message.server)
-        if state.is_playing():
-            player = state.player
-            player.volume = value / 100
-            embed1 = discord.Embed(color=color, description="• Volume definido para\n\n **{:.0%}**".format(player.volume))
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.say(embed=embed1)
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def pause(self, ctx):
-        state = self.get_voice_state(ctx.message.server)
-        if state.is_playing():
-            player = state.player
-            player.pause()
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def resume(self, ctx):
-        state = self.get_voice_state(ctx.message.server)
-        if state.is_playing():
-            player = state.player
-            player.resume()
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def stop(self, ctx):
-        server = ctx.message.server
-        state = self.get_voice_state(server)
-
-        if state.is_playing():
-            player = state.player
-            player.stop()
-
-        try:
-            state.audio_player.cancel()
-            del self.voice_states[server.id]
-            await state.voice.disconnect()
-        except:
-            pass
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def skip(self, ctx):
-        state = self.get_voice_state(ctx.message.server)
-        if not state.is_playing():
-            embed1 = discord.Embed(color=color, description="**• Não esta tocando música agora!**")
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.say(embed=embed1)
-            return
-
-        voter = ctx.message.author
-        if voter == state.current.requester:
-            embed1 = discord.Embed(color=color, description="**• Usuário solicitou pular á música!**")
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.say(embed=embed1)
-            state.skip()
-        elif voter.id not in state.skip_votes:
-            state.skip_votes.add(voter.id)
-            total_votes = len(state.skip_votes)
-            if total_votes >= 2:
-                embed1 = discord.Embed(color=color, description="**• Votação aprovada,pulando a música!**")
-                embed1.timestamp = datetime.datetime.utcnow()
-                await self.bot.say(embed=embed1)
-                state.skip()
-            else:
-                embed1 = discord.Embed(color=color, description="**• Votação para pular a musica, atualmente em [{}/2]**".format(total_votes))
-                embed1.timestamp = datetime.datetime.utcnow()
-                await self.bot.say(embed=embed1)
-        else:
-            embed1 = discord.Embed(color=color,description="**• Você votou para pular esta música!**")
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.say(embed=embed1)
-
-    @commands.command(pass_context=True, no_pm=True)
-    async def playing(self, ctx):
-        state = self.get_voice_state(ctx.message.server)
-        if state.current is None:
-            embed1 = discord.Embed(color=color,description="**• Nenhuma musica esta sendo tocada!**")
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.say(embed=embed1)
-        else:
-            skip_count = len(state.skip_votes)
-            embed1 = discord.Embed(color=color,description="**• Tocando agoran\n {} [skips: {}/3]**".format(state.current, skip_count))
-            embed1.timestamp = datetime.datetime.utcnow()
-            await self.bot.say(embed=embed1)
-
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('ls!'), description='Comandos Teste')
-bot.add_cog(Music(bot))
-
-
-@bot.event
+@client.event
 async def on_ready():
-    print('Música Online!')
-    print('Online Zero ---->', bot.user)
-    print('ID:', bot.user.id)
+    print(client.user.name)
+    print("===================")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('z!entrar'):
+        try:
+            channel = message.author.voice.voice_channel
+            await client.join_voice_channel(channel)
+        except discord.errors.InvalidArgument:
+            embed1 = discord.Embed(color=color,description="Desculpe senhor(a) já estou em um canal de voz")
+            embed1.set_footer(text=message.server.name, icon_url=message.server.icon_url)
+            embed1.timestamp = datetime.datetime.utcnow()
+            await client.send_message(message.channel,embed=embed1)
+        except Exception as error:
+            embed2 = discord.Embed(color=color,description="``{error}``".format(error=error))
+            embed2.set_footer(text=message.server.name, icon_url=message.server.icon_url)
+            embed2.timestamp = datetime.datetime.utcnow()
+            await client.send_message(message.channel,embed=embed2)
+
+    if message.content.startswith('z!sair'):
+        try:
+            mscleave = discord.Embed(title="\n",color=color,description="Hey senhor (a) sai do canal de voz e a música parou!")
+            mscleave.set_footer(text=message.server.name, icon_url=message.server.icon_url)
+            mscleave.timestamp = datetime.datetime.utcnow()
+            voice_client = client.voice_client_in(message.server)
+            await client.send_message(message.channel, embed=mscleave)
+            await voice_client.disconnect()
+        except AttributeError:
+            mscleave1 = discord.Embed(title="\n",color=color,description="Hey senhor (a) não estou em nenhum canal de voz.")
+            mscleave1.set_footer(text=message.server.name, icon_url=message.server.icon_url)
+            mscleave1.timestamp = datetime.datetime.utcnow()
+            await client.send_message(message.channel,ebemd=mscleavel1)
+        except Exception as Hugo:
+            embed3 = discord.Embed(color=color,description="Ein Error: ``{haus}``".format(haus=Hugo))
+            embed3.set_footer(text=message.server.name, icon_url=message.server.icon_url)
+            embed3.timestamp = datetime.datetime.utcnow()
+            await client.send_message(message.channel,embed=embed3)
+
+    if message.content.startswith('z!tocar'):
+        try:
+            yt_url = message.content[6:]
+            if client.is_voice_connected(message.server):
+                try:
+                    voice = client.voice_client_in(message.server)
+                    players[message.server.id].stop()
+                    player = await voice.create_ytdl_player('ytsearch: {}'.format(yt_url))
+                    players[message.server.id] = player
+                    player.start()
+                    mscemb = discord.Embed(title="\n",color=color)
+                    mscemb.add_field(name="Música",inline=True ,value="{}".format(player.title))
+                    mscemb.add_field(name="Views",inline=False, value="[]".format(player.views))
+                    mscemb.add_field(name="Postado Em",inline=True , value="{}".format(player.uploaded_date))
+                    mscemb.add_field(name="Canal",inline=True , value="{}".format(player.uploadeder))
+                    mscemb.add_field(name="Duração",inline=True , value="{}".format(player.uploadeder))
+                    mscemb.add_field(name="Likes",inline=True , value="{}".format(player.likes))
+                    mscemb.add_field(name="Deslikes",inline=True , value="{}".format(player.dislikes))
+                    mscemb.set_footer(text=message.server.name, icon_url=message.server.icon_url)
+                    mscemb.timestamp = datetime.datetime.utcnow()
+                    await client.send_message(message.channel, embed=mscemb)
+                except Exception:
+                    await client.send_message(message.server, "Error: [{error}]".format(error=e))
+
+            if not client.is_voice_connected(message.server):
+                try:
+                    channel = message.author.voice.voice_channel
+                    voice = await client.join_voice_channel(channel)
+                    player = await voice.create_ytdl_player('ytsearch: {}'.format(yt_url))
+                    players[message.server.id] = player
+                    player.start()
+                    mscemb2 = discord.Embed(title="\n",color=color)
+                    mscemb2.add_field(name="Música",inline=True ,value="``{}``".format(player.title))
+                    mscemb2.add_field(name="Views",inline=False, value="``[]``".format(player.views))
+                    mscemb2.add_field(name="Postado",inline=True , value="``{}``".format(player.upload_date))
+                    mscemb2.add_field(name="Canal",inline=True ,value="``{}``".format(player.uploader))
+                    mscemb2.add_field(name="Duração",inline=True , value="``{}``".format(player.duration))
+                    mscemb2.add_field(name="Likes",inline=True , value="``{}``".format(player.likes))
+                    mscemb2.add_field(name="Deslikes",inline=True , value="``{}``".format(player.dislikes))
+                    mscemb2.set_footer(text=message.server.name, icon_url=message.server.icon_url)
+                    mscemb2.timestamp = datetime.datetime.utcnow()
+                    await client.send_message(message.channel, embed=mscemb2)
+                except Exception:
+                    await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+        except Exception as e:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=e))
 
 
-bot.run(str(os.environ.get('TOKEN')))
+
+
+    if message.content.startswith('z!pausa'):
+        try:
+            mscpause = discord.Embed(title="\n",color=color,description="\n\n\n```Música pausada com sucesso!```")
+            mscpause.set_thumbnail(url="https://cdn.discordapp.com/emojis/485124116576337943.png?v=1")
+            mscpause.set_footer(text=message.server.name, icon_url=message.server.icon_url)
+            mscpause.timestamp = datetime.datetime.utcnow()
+            await client.send_message(message.channel, embed=mscpause)
+            players[message.server.id].pause()
+        except Exception as error:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+    if message.content.startswith('z!resume'):
+        try:
+            mscresume = discord.Embed(title="\n",color=color,description="\n\n\n```Música retornou a tocar com sucesso senhor (a)!```" )
+            mscresume.set_thumbnail(url="https://cdn.discordapp.com/emojis/485124116576337943.png?v=1")
+            mscresume.set_footer(text=message.server.name, icon_url=message.server.icon_url)
+            mscresume.timestamp = datetime.datetime.utcnow()
+            await client.send_message(message.channel, embed=mscresume)
+            players[message.server.id].resume()
+        except Exception as error:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+
+
+
+client.run(str(os.environ.get('TOKEN')))
